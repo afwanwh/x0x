d57f2795fefd5748b636b799c0555aa1e71410bc
Afwan Wali Hamim <afwan.hamim@infi-nity.com>
2015-02-12 11:11:27 +0700

    [FIX] change console dialog

--- gcd.py
+++ gcd.py
@@ -3,17 +3,31 @@
 
 
 def gcd(a, b):
-    num1 = a
-    num2 = b
-    while b > 0:
-        q = a / b
-        r = a - q * b
-        a = b
-        b = r
-    res = 'The GCD between ' + str(num1) + ' and ' + str(num2) + ' is ' + str(a)
+    if isinstance(a, int) and isinstance(b, int):
+        num1 = a
+        num2 = b
+        while b > 0:
+            q = a / b
+            r = a - q * b
+            a = b
+            b = r
+        res = 'The GCD between ' + str(num1) + ' and ' + str(num2) + ' is ' + str(a)
+    else:
+        res = 'One of your inputs is not integer'
     return res
 
-# Call the gcd(a,b) with input. For example like this one below.
+# Call the gcd(a,b) with input.
+print '##############################################################################'
+print 'Welcome to GCD 1.0!'
+print 'This program is for finding greatest common divisor.'
+print 'Input is two numbers, both of the are integers. Then it will return the gcd.'
+print '##############################################################################\n'
 number1 = raw_input('Type your first number: ')
 number2 = raw_input('Type your second number: ')
-print gcd(int(number1), int(number2))
+print '\n=============================================================================='
+try:
+    print gcd(int(number1), int(number2))
+except ValueError:
+    print 'One of your inputs is not integer.'
+    print 'Please give integer input(s).'
+print '=============================================================================='
