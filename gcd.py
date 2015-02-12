0ed8a57214f96ddaadfb6b728c1dcee7f3b584b5
Afwan Wali Hamim <afwan.hamim@infi-nity.com>
2015-02-12 10:48:24 +0700

    [FIX] change input method with raw_input

--- gcd.py
+++ gcd.py
@@ -1,7 +1,8 @@
 # This program is for finding greatest common divisor.
 # Input is a and b, both of the are integers. Then it will return the gcd.
 
-def gcd(a,b):
+
+def gcd(a, b):
     num1 = a
     num2 = b
     while b > 0:
@@ -13,18 +14,6 @@ def gcd(a,b):
     return res
 
 # Call the gcd(a,b) with input. For example like this one below.
-# gcd(15,9);
-
-# Or you could try this main program. Change a and b therefore run the code.
-a = 30 # Change this as your first input
-b = 3098647 # Change thisbas your second input
-num1 = a
-num2 = b
-while b > 0:
-    q = a / b
-    r = a - q * b
-    a = b
-    b = r
-res = 'The GCD between ' + str(num1) + ' and ' + str(num2) + ' is ' + str(a)
-print res 
-# This is the result
+number1 = raw_input('Type your first number: ')
+number2 = raw_input('Type your second number: ')
+print gcd(int(number1), int(number2))
